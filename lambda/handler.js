60bbb74d3814869fc3551c93ca0a29f01f7180c8
Ryan Walters <rwalters@eb.com>
2017-11-03 11:18:48 -0500

    functioning image resizing process on lambda, served from s3

--- lambda/handler.js
+++ lambda/handler.js
@@ -1,13 +1,56 @@
 'use strict';
 
-module.exports.resizeImage = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'resize image',
-      input: event,
-    }),
-  };
+const AWS = require('aws-sdk');
+const sharp = require('sharp');
 
-  callback(null, response);
+const S3 = new AWS.S3({ signatureVersion: 'v4' });
+
+module.exports.resizeImage = (event, context, callback) => {
+  const BUCKET = process.env.BUCKET;
+  const URL = process.env.URL;
+  const CDN_URL = process.env.CDN_URL;
+
+  const key = event.queryStringParameters.key;
+  const match = key.match(/(\d+)x(\d+)\/(.*)/);
+  const height = parseInt(match[1]);
+  const width = parseInt(match[2]);
+  const imagePath = match[3];
+
+  console.log('started');
+
+  S3.getObject({ Bucket: BUCKET, Key: imagePath })
+    .promise()
+    .then(data => {
+      console.log('fetched image, transforming');
+
+      return sharp(data.Body)
+        .resize(width, height)
+        .crop(sharp.strategy.attention)
+        .toFormat('jpg')
+        .toBuffer()
+    })
+    .then(buffer => {
+      console.log('transformed');
+
+      return S3.putObject({
+        ACL: 'public-read',
+        Body: buffer,
+        Bucket: BUCKET,
+        CacheControl: 'public, max-age=31536000',
+        ContentType: 'image/jpg',
+        Key: key,
+      }).promise();
+    })
+    .then(() => {
+      console.log('object uploaded:', key);
+
+      return callback(null, {
+        statusCode: '301',
+        headers: {
+          Location: `${URL}/${key}`,
+        },
+        body: '',
+      });
+    })
+    .catch(err => callback(err));
 };
